c569ea2cc3716fc637985fb6aa4efb0ef0695221
Phatthie <younefayya@gmail.com>
2026-02-11 18:55:29 +0700

    1888888

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -10,3 +10,22 @@ double overlap(){
 
 
 }
+#include<iostream>
+using namespace std;
+
+struct Rect{
+	double x,y,w,h;
+};
+
+double overlap(Rect r1,Rect r2){
+    double left = max(r1.x,r2.x);
+    double right = min(r1.x+r1.w,r2.x+r2.w);
+    double bottom = max(r1.y-r1.h,r2.y-r2.h);
+    double top = min(r1.y,r2.y);
+    
+    double width = right - left;
+    double height = top - bottom;
+    
+    if(width<0 || height<0) return 0;
+    else return width*height;
+}
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *r1,Rect *r2){
+    const double left = max(r1->x,r2->x);
+    const double right = min(r1->x+r1->w,r2->x+r2->w);
+    const double bottom = max(r1->y-r1->h,r2->y-r2->h);
+    const double top = min(r1->y,r2->y);
+    
+    double width = right - left;
+    double height = top - bottom;
+    
+    if(width<0 || height<0) return 0;
+    else return (width)*(height);
 }
